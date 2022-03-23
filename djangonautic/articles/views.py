512f18b99c258c52063f6ce391871e07b24b164c
Hamisi-Esq <brivahamisi@gmail.com>
2022-03-24 00:24:41 +0300

    Latest Update Version

--- djangonautic/articles/views.py
+++ djangonautic/articles/views.py
@@ -3,5 +3,5 @@ from .models import Article
 
 # Create your views here.
 def article_list(request):
-    articles = Article.objects.all
-    return render(request,'articles/article_list.html')
+    articles = Article.objects.all().order_by("date")
+    return render(request,'articles/article_list.html', {'articles': articles})
